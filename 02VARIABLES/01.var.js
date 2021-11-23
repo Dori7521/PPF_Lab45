2fd4f02949136b1270540ebacba4f6f9e6faf37b
Dori7521 <lelitodorota@gmail.com>
2021-11-23 10:27:23 +0100

    plik z var w js

---
+++ 02VARIABLES/01.var.js
@@ -0,0 +1,28 @@
+var x= 4;
+var y =6;
+var z=x+y;
+
+var name = "Dorota";
+var person = "Jan Kowalski", carName = "Nissan", price = 200000;
+
+var personA = "Jan Kowalski";
+carNameA = "Nissan";
+priceA = 200000;
+
+// Zmienna zadeklarowana bez wartości będzie miała wartość undofined.
+var carNameB;
+var personX = "Janina" + " "+"Nowak";
+
+var isFamous = true;  //False
+
+var xx = 2+3 +"5"
+
+console.log(x,y,z,xx)
+console.log(name)
+console.log(person,personA,personX)
+console.log(carName,carNameA,carNameB)
+console.log(priceA)
+console.log(isFamous)
+
+
+
