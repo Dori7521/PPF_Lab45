653afdf82188f3affbc4a8af14c5f0f19d79f471
Dori7521 <lelitodorota@gmail.com>
2021-11-23 10:38:54 +0100

    arraws cd

---
+++ 03MUST/01.arrows.js
@@ -0,0 +1,16 @@
+const getFavouriteNumber = () => 6;
+const addSeven = a => a +7;
+const divide = (a,b) => a/b
+
+//powyższy kod działa tak samo jak:
+function getFavouriteNumber(){
+    return 6;
+}
+
+function addSeven(a){
+    return a+7;
+}
+
+function divide(a,b){
+    return a/b;
+}
